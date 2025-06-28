bab189b1d90dc42093958ece007ba0b0f2ef2eb2
Sebastian Milczarek <sebastian.milczarek12@gmail.com>
2025-06-28 11:31:05 +0200

    project initialization

---
+++ src/main/java/com/example/windsurfers_weather/WindsurfersWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.windsurfers_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfersWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurfersWeatherApplication.class, args);
+	}
+
+}
